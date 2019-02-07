99fe4c81e4b3379a16f9b58cbc889defdfecb9bf
Jonathan Adler <jonathana@wix.com>
2019-02-07 19:37:01 +0200

    remove comments

--- stories/FloatingNotification/examples/index.js
+++ stories/FloatingNotification/examples/index.js
@@ -7,32 +7,3 @@ export default {
   ExampleAllOptions,
   ExampleHref,
 };
-
-// import React from 'react';
-// import LiveCodeExample from '../../utils/Components/LiveCodeExample';
-// import { Layout, Cell } from '../../../src/Layout';
-//
-//
-// export default class FloatingNotificationExample extends React.Component {
-//   state = {
-//     examples: [
-//       { example: ExampleAllTypes, title: 'All types' },
-//       { example: ExampleAllOptions, title: 'All options' },
-//       { example: ExampleHref, title: 'TextButton as anchor with href' },
-//     ],
-//   };
-//
-//   render() {
-//     const { examples } = this.state;
-//
-//     return (
-//       <Layout>
-//         {examples.map(({ example, title }) => (
-//           <Cell span={12} key={title}>
-//             <LiveCodeExample compact title={title} initialCode={example} />
-//           </Cell>
-//         ))}
-//       </Layout>
-//     );
-//   }
-// }
